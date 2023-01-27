80e52984aa9dbba65adee5ec64736527bdeb5672
Azamat Home <Azamat@Home>
2023-01-27 20:35:29 +0600

    cartContext

---
+++ src/context/ShopingCartContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+type ShopingCartProviderProps = {
+  children: ReactNode;
+};
+type ShopingCartContext = {
+  getitemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+const ShopingCartContext = createContext({} as ShopingCartContext);
+
+export function useShopingCartContext() {
+  return useContext(ShopingCartContext);
+}
+
+export function ShopingCartProvider({ children }: ShopingCartProviderProps) {
+  const [cartItmes, setCartItems] = useState<CartItem[]>([]);
+
+  return (
+    <ShopingCartContext.Provider value={{}}>
+      {children}
+    </ShopingCartContext.Provider>
+  );
+}
