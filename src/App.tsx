e278d23f85c0760a02462217a74ca357884f9871
Azamat Home <Azamat@Home>
2023-01-30 20:27:31 +0600

    deploy to a vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
@@ -12,8 +11,7 @@ function App() {
       <Navbar />
       <Container>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
+          <Route path="/" element={<Store />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,15 @@
+import { Container } from "react-bootstrap";
+
 export function About() {
   return (
     <div>
       <h1>About</h1>
+      <Container>
+        <p>
+          This is a simple store template in React + Context API + TypeScript +
+          Bootstrap. Only for a portfolio.
+        </p>
+      </Container>
     </div>
   );
 }
